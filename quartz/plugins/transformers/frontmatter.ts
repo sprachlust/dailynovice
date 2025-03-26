e8c4e3f363c0000d1a58b1b0b9e7cc0b4503f106
우 성걸 <ianwoo@makestarcorp.onmicrosoft.com>
2025-03-26 15:16:02 +0900

    Quartz sync: Mar 26, 2025, 3:16 PM

--- quartz/plugins/transformers/frontmatter.ts
+++ quartz/plugins/transformers/frontmatter.ts
@@ -96,7 +96,7 @@ export const FrontMatter: QuartzTransformerPlugin<Partial<Options>> = (userOpts)
 
             const socialImage = coalesceAliases(data, ["socialImage", "image", "cover"])
 
-            const created = coalesceAliases(data, ["created", "date"])
+            const created = coalesceAliases(data, ["date"])
             if (created) data.created = created
             const modified = coalesceAliases(data, [
               "modified",
@@ -105,7 +105,7 @@ export const FrontMatter: QuartzTransformerPlugin<Partial<Options>> = (userOpts)
               "last-modified",
             ])
             if (modified) data.modified = modified
-            const published = coalesceAliases(data, ["published", "publishDate", "date"])
+            const published = coalesceAliases(data, ["date"])
             if (published) data.published = published
 
             if (socialImage) data.socialImage = socialImage
